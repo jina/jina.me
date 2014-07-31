9cde728883015a31342d7b626eb0c351a37f4bb8
Jina Bolton <jina@sushiandrobots.com>
2014-07-30 22:10:16 -0700

    layout system

--- config.rb
+++ config.rb
@@ -1,29 +1,48 @@
 require 'breakpoint'
 require "builder"
+require 'middleman-syntax'
 require 'susy'
 require 'typogruby'
 
 activate :directory_indexes
-activate :livereload
-activate :relative_assets
+activate :syntax
 
+set :markdown, :fenced_code_blocks => true,
+               :autolink => true,
+               :smartypants => true
+set :markdown_engine, :redcarpet
 set :css_dir,    'assets/css'
 set :js_dir,     'assets/js'
 set :images_dir, 'assets/img'
-set :relative_links, true
-
-compass_config do |config|
-  config.output_style = :expanded
-end
 
 page "/humans.txt", :layout => false
+page "/index.html", :layout => :layout_2_column
+
+with_layout :styleguide do
+  page "/styleguide/*"
+end
+
+
+
+configure :development do
+  activate :livereload
+
+  compass_config do |config|
+    config.output_style = :expanded
+  end
+end
+
+
 
 configure :build do
-  activate :minify_css
-  activate :minify_javascript
-  activate :minify_html
-  #activate :asset_hash
+  activate :asset_hash
   activate :gzip
+  activate :minify_css
+  activate :minify_html
+  activate :minify_javascript
+  activate :relative_assets
+
+  set :relative_links, true
 
   compass_config do |config|
     config.output_style = :compressed
