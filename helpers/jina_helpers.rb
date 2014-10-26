7a76a54742425dc45181c184487d832831189c2f
Jina Bolton <jina@sushiandrobots.com>
2014-10-25 23:51:45 -0700

    Nicer contact form

--- helpers/jina_helpers.rb
+++ helpers/jina_helpers.rb
@@ -11,6 +11,10 @@ module JinaHelpers
     title
   end
 
+  def button_tag(text, options={})
+    content_tag(:button, {:type => "submit"}.merge(options)) { text }
+  end
+
   def copyright_years(start_year)
     end_year = Date.today.year
     if start_year == end_year
--- source/assets/js/jina.js
+++ source/assets/js/jina.js
@@ -1,3 +1,6 @@
 //= require ./vendor/jquery-2.1.1
 //= require ./vendor/plugins
 //= require "vendor/jquery.balancetext"
+//= require "vendor/jquery.validate"
+
+//= require "validate"
